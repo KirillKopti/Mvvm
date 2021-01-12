b95eb554a74bd6767877eb154799b63a9d6287f0
Kirill <Kiril@PROMETHEUS>
2021-01-12 09:25:51 +0200

    6

---
+++ Mvvm/Mvvm/Views/FriendPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mvvm.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FriendPage : ContentView
+    {
+        public FriendPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
