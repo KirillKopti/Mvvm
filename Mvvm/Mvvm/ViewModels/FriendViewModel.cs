3f3caaf679ccaee9029e97ef399abc94ab18a201
Kirill <Kiril@PROMETHEUS>
2021-01-12 22:21:19 +0200

    5

--- Mvvm/Mvvm/ViewModels/FriendViewModel.cs
+++ Mvvm/Mvvm/ViewModels/FriendViewModel.cs
@@ -38,6 +38,39 @@ namespace Mvvm.ViewModels
                 }
             }
         }
+        public string Email
+        {
+            get { return Friend.Email; }
+            set
+            {
+                if (Friend.Email != value)
+                {
+                    Friend.Email = value;
+                    OnPropertyChanged("Email");
+                }
+            }
+        }
+        public string Phone
+        {
+            get { return Friend.Phone; }
+            set
+            {
+                if (Friend.Phone != value)
+                {
+                    Friend.Phone = value;
+                    OnPropertyChanged("Phone");
+                }
+            }
+        }
+        public bool IsValid
+        {
+            get
+            {
+                return ((!string.IsNullOrEmpty(Name.Trim))) ||
+                    ((!string.IsNullOrEmpty(Phone.Trim)))  ||
+                    ((!string.IsNullOrEmpty(Email.Trim)));
+            }
+        }
 
     }
 }
