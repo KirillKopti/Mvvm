5fcfd72a8ef743978d199f35527772d716bdf433
Kirill <Kiril@PROMETHEUS>
2021-01-12 12:03:30 +0200

    6

--- Mvvm/Mvvm/ViewModels/FriendViewModel.cs
+++ Mvvm/Mvvm/ViewModels/FriendViewModel.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using System.ComponentModel;
 using System.Collections.Generic;
 using System.Text;
+using Mvvm.Models;
 
 namespace Mvvm.ViewModels
 {
-    class FriendViewModel
+    public class FriendViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        FriendsListViewModel lvm;
+        public Friend Friend { get; private set; }
+        public FriendViewModel()
+        {
+            Friend = new Friend();
+        }
+
     }
 }
