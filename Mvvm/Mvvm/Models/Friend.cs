27f2079042d9f0131dbc93485761fe5bcba4dc4b
Kirill <Kiril@PROMETHEUS>
2021-01-12 10:05:46 +0200

    3

--- Mvvm/Mvvm/Models/Friend.cs
+++ Mvvm/Mvvm/Models/Friend.cs
@@ -4,7 +4,10 @@ using System.Text;
 
 namespace Mvvm.Models
 {
-    class Friend
+    public class Friend
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
