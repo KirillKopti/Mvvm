1103abe69920d46a64d2ea40d9a6e395ca5d4e85
Kirill <Kiril@PROMETHEUS>
2021-01-14 20:07:58 +0200

    5

--- Mvvm/Mvvm/Views/FriendsListPage.xaml.cs
+++ Mvvm/Mvvm/Views/FriendsListPage.xaml.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Mvvm.ViewModels;
 
 namespace Mvvm.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FriendsListPage : ContentView
     {
         public FriendsListPage()
         {
             InitializeComponent();
+            BindingContext = new FriendsListViewModel() { Navigation = this.Navigation };
         }
     }
 }
